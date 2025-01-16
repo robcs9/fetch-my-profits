71f1bff88ed5232e08a0266b9ff6d58c59c6c0f8
robcs9 <83130807+robcs9@users.noreply.github.com>
2025-01-15 22:51:43 -0300

    Updating model class and initial data modeling with DataFrames

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import pandas
 from models.model import Operation
 
+# def getTickerOps(sheet_df: pandas.DataFrame, ticker: str):
+  # return sheet_df[sheet_df['ticker'] == ticker]
+
 def readSheet(filePath):
-  return pandas.read_csv(filePath, encoding='utf-8')
+  sheet = pandas.read_csv(filePath, encoding='utf-8')
+  return sheet
+
 def main():
   sheet = readSheet('./carteira.csv')
+  operations = Operation.buildOperations(sheet)
+  # print(vars(operations[0]))
+  # print([vars(op) for op in operations])
+  
+  consolidations = []
   oprts = sheet[sheet['ticker'] == 'MGLU3']
+  # oprts = getTickerOps(sheet,'MGLU3')
+  
+  def getTickerOps(oprts: list[Operation], ticker: str):
+    ops = []
+    for op in oprts:
+      if op.ticker == ticker:
+        ops.append(op)
+    return ops
+  
+  print([vars(el) for el in getTickerOps(operations, 'MGLU3')])
   # 
   initial_op = oprts.iloc[0]
+  
   for i, op in enumerate(oprts):
     if i == 0:
       continue
     if op['type'] != initial_op['type'] and initial_op['type'] == 'C':
       # calc profit/loss
       result = initial_op['price'] - op['price']
+      consolidated = True
       pass
     elif op['type'] != initial_op['type'] and initial_op['type'] == 'V':
       result = -initial_op['price'] + op['price']
--- models/model.py
+++ models/model.py
@@ -1,7 +1,9 @@
+import pandas
+
 class Operation:
-  result = "" # "Profit" or "Loss"
+  result = '' # "Profit" or "Loss"
   
-  def __init__(self, type, date, ticker, isConsolidated = False, price = 0, tax = 0):
+  def __init__(self, type='C/V', date='dd/mm/aaaa', ticker='TICK3R', quantity=0, price = 0, tax = 0, isConsolidated = False):
     self.type = type # "Sell" or "Buy"
     self.price = price
     self.date = date
@@ -9,4 +11,22 @@ class Operation:
     # self.trade_costs
     self.isConsolidated = isConsolidated
     self.tax = tax
-    pass
+    self.quantity = quantity
+    self.total = self.quantity * self.price
+    # self.name = ""
+  
+  @staticmethod
+  def buildOperations(sheet: pandas.DataFrame):
+    ops_count = sheet.count(axis=0).iloc[0]
+    operations = []
+    for i in range(ops_count):
+
+      op = Operation(
+        date=sheet.iloc[i]['date'],
+        ticker=sheet.iloc[i]['ticker'],
+        type=sheet.iloc[i]['operation'],
+        price=sheet.iloc[i]['price'],
+        quantity=int(sheet.iloc[i]['quantity']),
+      )
+      operations.append(op)
+    return operations
